bcb6b205983f9927a5fd1917ee3fe57f7bd72cca
Grzetan <grzetan@gmail.com>
2023-04-18 20:18:55 +0200

    Added function to render text

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,30 @@
 #define W 1000
 #define H 800
 
-void renderText(){
+void get_text_and_rect(SDL_Renderer *renderer, int x, int y, const char *text,
+        TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect) {
+    int text_width;
+    int text_height;
+    SDL_Surface *surface;
+    SDL_Color textColor = {255, 255, 255, 0};
 
+    surface = TTF_RenderText_Solid(font, text, textColor);
+    *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    text_width = surface->w;
+    text_height = surface->h;
+    SDL_FreeSurface(surface);
+    rect->x = x;
+    rect->y = y;
+    rect->w = text_width;
+    rect->h = text_height;
 }
+
 
 int main(int argc, char *argv[]){
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
+    TTF_Init();
     SDL_Window* win = SDL_CreateWindow("Battleships game",
                                        SDL_WINDOWPOS_CENTERED,
                                        SDL_WINDOWPOS_CENTERED,
@@ -32,10 +48,15 @@ int main(int argc, char *argv[]){
     SDL_Renderer* renderer = NULL;
     renderer =  SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24);
+    TTF_Font* font = TTF_OpenFont("./fonts/VertigoFLF.ttf", 50);
+
+    SDL_Texture *texture1;
+    SDL_Rect rect;
+
+    get_text_and_rect(renderer, 200, 200, "Hello World", font, &texture1, &rect);
 
     while (!close) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         SDL_Rect r;
@@ -47,6 +68,8 @@ int main(int argc, char *argv[]){
         SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
 
         SDL_RenderFillRect( renderer, &r );
+
+        SDL_RenderCopy(renderer, texture1, NULL, &rect);
 
         SDL_RenderPresent(renderer);
 
